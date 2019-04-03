3c79f3a29c2effae4b576c392f23a4a3e11aaa18
robjweiss <rweiss55@gmail.com>
2019-04-03 16:04:32 -0400

    Added year, rating, desc, and styling to results

--- screens/ResultsScreen.js
+++ screens/ResultsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Text, ActivityIndicator, FlatList } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator, FlatList, Image, TouchableNativeFeedback, ImageBackground } from 'react-native';
 import tmdb from '../config/tmdb';
 const apiKey = tmdb.apiKey;
 
@@ -11,6 +11,12 @@ export default class ResultsScreen extends React.Component {
             isLoading: true
         };
     }
+
+    static navigationOptions = ({ navigation }) => {
+        return {
+          title: "Results for '" + navigation.getParam('searchTerm', 'Results') + "'"
+        };
+      };
 
     async componentDidMount() {
         const { navigation } = this.props;
@@ -43,21 +49,77 @@ export default class ResultsScreen extends React.Component {
           }
 
         return (
-            <View>
+            <ImageBackground
+                source = {require('../assets/background.jpg')}
+                style = {styles.background}
+            >
                 <FlatList
                     data = {this.state.results}
-                    renderItem = {({item}) => <Text>
-                        {item.title}
-                    </Text>}
+                    renderItem = {({item}) => <TouchableNativeFeedback onPress = {() => this.props.navigation.navigate('Movie', {
+                        id: item.id,
+                        posterPath: item.poster_path
+                    })}>
+                        <View style = {styles.resultItem}>
+                            <Image
+                                source = {{uri: 'https://image.tmdb.org/t/p/w154/' + item.poster_path}}
+                                style = {styles.posterImages}
+                            />
+                            <View style = {styles.resultTextContainer}>
+                                <Text style = {styles.title}>{item.title}</Text>
+                                <Text style = {styles.subheading}>Year: {item.release_date.slice(0, 4)} |  Rating: {item.vote_average}</Text>
+                                <Text
+                                    numberOfLines = {3}
+                                    style = {styles.overview}
+                                >{item.overview}
+                                </Text>
+                            </View>
+                        </View>
+                    </TouchableNativeFeedback>
+                    }
                     keyExtractor = {(item, index) => item.id.toString()}
                 />
-            </View>
+            </ImageBackground>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
+    background: {
+        width: '100%',
+        height: '100%'
+    },
+    resultItem: {
+        flex: 1,
+        flexDirection: 'row',
+        marginLeft: 15,
+        marginRight: 15,
+        marginTop: 5,
+        marginBottom: 5,
+        backgroundColor: 'rgba(255, 255, 255, 1.0)'
+    },
+    posterImages: {
+        width: 92,
+        height: 138
+    },
+    resultTextContainer: {
+        flex: 1,
+        flexWrap: 'wrap',
+    },
+    title: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginLeft: 10,
+        marginTop: 10
+    },
+    subheading: {
+        color: 'slategray',
+        fontStyle: 'italic',
+        marginLeft: 10
+    },
+    overview: {
+        color: 'slategray',
+        position: 'absolute',
+        bottom: 0,
+        margin: 15
     }
 });
